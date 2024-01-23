28d56cfe275eff446b2e96c3c18451e854853b1b
gbambalan <156237558+gbambalan@users.noreply.github.com>
2024-01-23 13:34:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+document.getElementById(img="Rome").addEventListener("click", () => {
+    alert("Friends and me in Rome!");
+  });
+document.getElementById("Billionaires Bay").addEventListener("click", () => {
+    alert("Billionaires Bay in Antibes, France");
+  });
+document.getElementById("Youtube").addEventListener("click", function() {
+    window.location.href = "https://www.youtube.com/watch?v=QrR_gm6RqCo"; // Replace with your desired link
+  });
+
+  // image changing //
+  const myImage = document.getElementById("Billionaires Bay");
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/AntibesBB.png") {
+    myImage.setAttribute("src", "images/Swimming.png");
+  } else {
+    myImage.setAttribute("src", "images/AntibesBB.png");
+  }
+};
+
+  
+  
+
+  
