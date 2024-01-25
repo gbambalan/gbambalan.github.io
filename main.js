e6242cbfe29eeb8688e48c760d47123cb029ca0c
gbambalan <156237558+gbambalan@users.noreply.github.com>
2024-01-24 20:38:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ document.getElementById("Billionaires Bay").addEventListener("click", () => {
     alert("Billionaires Bay in Antibes, France");
   });
 document.getElementById("Youtube").addEventListener("click", function() {
-    window.location.href = "https://www.youtube.com/watch?v=QrR_gm6RqCo"; // Replace with your desired link
+    window.location.href = "https://www.youtube.com/watch?v=QrR_gm6RqCo";
   });
 
-  // image changing //
   const myImage = document.getElementById("Billionaires Bay");
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
@@ -18,6 +17,31 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "AntibesBB.png");
   }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h2");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Nice to meet you ${myName}!`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Nice to meet you ${storedName}!`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
+
 
   
   
